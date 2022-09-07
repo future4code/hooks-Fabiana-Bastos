f3cbc45e4023b753bee0c1e25c5f323b62a5bf16
fabidbastos <fabidbastos@gmail.com>
2022-09-07 14:21:06 -0300

    mod intro-express

--- modulo5/intro-express/src/index.ts
+++ modulo5/intro-express/src/index.ts
@@ -1,55 +1,45 @@
 import express from 'express'
-import { Request, Response } from 'express'
 import cors from 'cors'
 
-//EXERCÍCIO 1
 const app = express()
-
-app.listen(3003, () => {
-    console.log('Hi there!');
- });
-
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (request: Request, response: Response) => {
-    response.send('Bem vinde!'); 
- });
- 
- app.get('/documentation', (request: Request, response: Response) => {
-    response.send('Alô alô Brasil'); 
- });
+app.listen(3003, () => console.log("Servidor rodando na porta 3003"))
 
- app.get('/about', (request: Request, response: Response) => {
-    response.send('Me chamo Fabiana');
- });
+//EXERCICIO1
+app.get('/', (req, res) => {
+    res.send('hello there')
+})
 
- //EXERCÍCIO 2
+app.get('/about', (req, res) => {
+    res.send('its me, fabiana')
+})
 
+//EXERCÍCIO2
+type Data = {
+    id: string | number,
+    name: string,
+    phone: string| number,
+    email: string,
+    website: string
+}
 
- type Data = {
-     id: string | number,
-     name: string,
-     phone: string| number,
-     email: string,
-     website: string
- }
+//EXERCÍCIO 3
 
- //EXERCÍCIO 3
-
- const users:Data[] = [
-	{ id: 1, name: "Bethânia", phone: 12345678, email: "bet@gmail.com", website:"bet.com.br" },
-    { id: 2, name: "Caetano", phone: 87654321, email: "cae@gmail.com", website:"cae.com.br" },
-	{ id: 3, name: "Gilberto", phone: 10101010, email: "gil@gmail.com", website:"gil.com.br" },
-	{ id: 4, name: "Gal", phone: 74758566, email: "gal@gmail.com", website:"gal.com.br" },
-    { id: 5, name: "Rita", phone: 5556666, email: "riri@gmail.com", website:"riri.com.br" },
+const users:Data[] = [
+	{ id: 1, name: "João", phone: 99998888, email: "joao@gmail.com", website:"joao.com.br" },
+    { id: 2, name: "Lucas", phone: 77776666, email: "lucas@gmail.com", website:"lucas.com.br" },
+	{ id: 3, name: "Renata", phone: 55554444, email: "renata@gmail.com", website:"renata.com.br" },
+	{ id: 4, name: "Dalila", phone: 33332222, email: "dalila@gmail.com", website:"dalila.com.br" },
+    { id: 5, name: "Dona B", phone: 22221111, email: "DonaB@gmail.com", website:"donaB.com.br" },
 ]
 
 console.log(users[1].name)
 
 //EXERCICIO 4
 
-app.get("/users", (req: Request, res: Response) => {
+app.get("/users", (req, res) => {
     if(!users.length){
         res.status(401).send("Vazio")
     }
@@ -58,8 +48,6 @@ app.get("/users", (req: Request, res: Response) => {
 }) 
 
 //EXERCICIO 5 
-
-
 type Posts = {
     id: number,
     title: string,
@@ -70,16 +58,17 @@ type Posts = {
 //EXERCICIO 6
 
 const postsData:Posts[] = [
-	{ id: 1, title: "abc", body: "óh abelha rainha", userId: 1,  },
-    { id: 2, title: "def", body: "faz de mim", userId:1, },
-	{ id: 3, title: "ghi", body: "um instrumento de", userId: 1, },
-	{ id: 4, title: "klm", body: "teu prazer, sim, sim", userId: 2, },
-    { id: 5, title: "nop", body: "e de tua glória", userId: 2, },
+	{ id: 1, title: "abc", body: "efg", userId: 1,  },
+    { id: 2, title: "hij", body: "klm", userId:1, },
+	{ id: 3, title: "nop", body: "qrs", userId: 1, },
+	{ id: 4, title: "tuv", body: "xyz", userId: 2, },
+    { id: 5, title: "ahhaha", body: "hehehe", userId: 2, },
 ]
+//EU PREFERI CRIAR FORA.
 
 // EXERCICIO 7
 
-app.get("/posts", (req: Request, res: Response) => {
+app.get("/posts", (req, res) => {
     if(!postsData.length){
         res.status(401).send("Vazio")
     }
@@ -89,7 +78,7 @@ app.get("/posts", (req: Request, res: Response) => {
 
 // EXERCICIO 8
 
-app.get("/users/posts", (req: Request, res: Response) => {
+app.get("/users/posts", (req, res) => {
 
     if(req.query.userId){
         console.log(req.query)
